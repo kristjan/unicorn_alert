7ee54d767870d095e3b2de42a8d579885535a6b0
Noah Silas <noah@silas.cc>
2012-05-05 20:56:11 -0700

    geocorns sighting data includes shiny categories

--- app/controllers/geocorn_controller.rb
+++ app/controllers/geocorn_controller.rb
@@ -1,5 +1,16 @@
 class GeocornController < ApplicationController
+  NICE_NAMES = Hash.new('sparkles').update({
+    'LARCENY/THEFT' => 'rainbows',
+    'OTHER OFFENSES' => 'glitter',
+    'NON-CRIMINAL' => 'unihorns',
+    'ASSAULT' => 'awesome',
+    'DRUG/NARCOTIC' => 'magic-rhino',
+    'VANDALISM' => 'land-narwhal',
+    'WARRANTS' => 'poop',
+  })
+
   def sightings
-    render :json => Crime.crimes[0..1000].map{|crime| crime['Location'][1..2]}
+    render :json => Crime.crimes[0..1000].map{|crime| crime['Location'][1..2] + [NICE_NAMES[crime['Category']]]}
   end
+
 end
